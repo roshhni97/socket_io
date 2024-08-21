b3171fec03d80842f4dc08ceed2bb39c9b500d40
Roshani9731 <roshaninavdiya9731@gmail.com>
2024-08-21 21:43:36 +0530

    feature: broadcasting using io.emit

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const io = new Server(server);
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
+
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("chat message", (msg) => {
-    console.log("Message: " + msg);
+    // console.log("Message: " + msg);
+    io.emit("chat message", msg);
   });
 });
 
